a7b030ffe8b82890b5a116e3afe2f5307c439e67
rodineiriboli <rodineiriboli@hotmail.com>
2019-03-25 23:21:04 -0300

    initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,102 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormGroup, Validators, FormControl, FormBuilder } from '@angular/forms';
+import { trigger, state, style, transition, animate, AnimationEvent } from '@angular/animations';
+import { MessageService, SelectItem } from 'primeng/api';
+
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('animation', [
+      state('visible', style({
+        transform: 'translateX(0)',
+        opacity: 1
+      })),
+      transition('void => *', [
+        style({ transform: 'translateX(50%)', opacity: 0 }),
+        animate('300ms ease-out')
+      ]),
+      transition('* => void', [
+        animate(('250ms ease-in'), style({
+          height: 0,
+          opacity: 0,
+          transform: 'translateX(50%)'
+        }))
+      ])
+    ])
+  ],
+  providers: [MessageService],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-  title = 'Store';
+export class AppComponent implements OnInit {
+
+  //unidade de medida
+  name: string;
+  code: string;
+  unidadesDeMedida: SelectItem[];
+
+  userform: FormGroup;
+
+  submitted: boolean;
+
+  description: string;
+  columns: number[];
+
+  //checkBox
+  checkBoxValue: boolean;
+
+  private breadcrumbs: MenuItem[];
+  private itemsMenu: MenuItem[];
+
+  constructor(private fb: FormBuilder, private messageService: MessageService) { }
+  
+  ngOnInit() {
+
+    //inicialização de unidade de medida
+    this.unidadesDeMedida = [
+      {label:'Selecione', value:''},
+      {label:'Litro', value:{id:1, name: 'Litro', code: 'lt'}},
+      {label:'Quilograma', value:{id:2, name: 'Quilograma', code: 'kg'}},
+      {label:'Unidade', value:{id:3, name: 'Unidade', code: 'un'}}
+  ];
+
+  //validação do formulário
+    this.userform = this.fb.group({
+      'nomeItem': new FormControl('', Validators.required),
+      'unidadeMedida': new FormControl('', Validators.required),
+      'preco': new FormControl('', Validators.required),
+      // 'lastname': new FormControl('', Validators.required),
+      // 'password': new FormControl('', Validators.compose([Validators.required, Validators.minLength(6)])),
+      // 'description': new FormControl(''),
+    });
+
+    //navegação estrutural
+    this.breadcrumbs = [
+      { label: 'New', icon: 'pi pi-fw pi-plus' },
+      { label: 'Open', icon: 'fa fa-list', url: 'https://en.wikipedia.org/wiki/Lionel_Messi' }
+    ];
+
+    //menu
+    this.itemsMenu = [
+      { label: 'Cadastro', icon: 'pi pi-fw pi-plus' },
+      { label: 'Listagem', icon: 'fa fa-list' }
+    ];
+  }
+  onSubmit(value: string) {
+    this.submitted = true;
+    this.messageService.add({ severity: 'info', summary: 'Success', detail: 'Form Submitted' });
+  }
+
+  get diagnostic() { return JSON.stringify(this.userform.value); }
+
+  addColumn() {
+    this.columns.push(this.columns.length);
+  }
+
+  removeColumn() {
+    this.columns.splice(-1, 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CardModule} from 'primeng/card';
+import {BreadcrumbModule} from 'primeng/breadcrumb';
+import {MenuModule} from 'primeng/menu';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ToastModule} from 'primeng/toast';
+import {MessageModule} from 'primeng/message';
+import {DropdownModule} from 'primeng/dropdown';
+import {PanelModule} from 'primeng/panel';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {InputTextModule} from 'primeng/inputtext';
+import {InputMaskModule} from 'primeng/inputmask';
+import {CheckboxModule} from 'primeng/checkbox';
+import {CalendarModule} from 'primeng/calendar';
 
 @NgModule({
   declarations: [
@@ -10,7 +23,21 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule,
+    BreadcrumbModule,
+    MenuModule,
+    BrowserAnimationsModule,
+    ToastModule,
+    MessageModule,
+    DropdownModule,
+    PanelModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    InputMaskModule,
+    CheckboxModule,
+    CalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
