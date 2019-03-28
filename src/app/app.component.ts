26a390afa66a4844a1144a0a82aa7aabee24b8d0
Rodinei Riboli <rodinei.riboli@hbsis.com.br>
2019-03-28 00:47:40 -0300

    modularizaçao inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,86 +9,27 @@ import { LocalStore } from './shared/localStore.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('animation', [
-      state('visible', style({
-        transform: 'translateX(0)',
-        opacity: 1
-      })),
-      transition('void => *', [
-        style({ transform: 'translateX(50%)', opacity: 0 }),
-        animate('300ms ease-out')
-      ]),
-      transition('* => void', [
-        animate(('250ms ease-in'), style({
-          height: 0,
-          opacity: 0,
-          transform: 'translateX(50%)'
-        }))
-      ])
-    ])
-  ],
+   styleUrls: ['./app.component.css'],
+
   providers: [MessageService],
   encapsulation: ViewEncapsulation.None
 })
 
-@Injectable()
 export class AppComponent implements OnInit {
 
   private breadcrumbs: MenuItem[];
   private itemsMenu: MenuItem[];
 
-  //unidade de medida
-  name: string;
-  code: string;
-  unidadesDeMedida: SelectItem[];
+  constructor(private localStore: LocalStore) {
 
-  userform: FormGroup;
-
-  submitted: boolean;
-
-  description: string;
-  columns: number[];
-
-  //checkBox
-  checkBoxValue: boolean;
-
-
-  constructor(private fb: FormBuilder,
-    private messageService: MessageService,
-    private localStore: LocalStore) { }
+  }
 
   ngOnInit() {
 
-    //inicialização de unidade de medida
-    this.unidadesDeMedida = [
-      { label: 'Selecione', value: '' },
-      { label: 'Litro', value: { id: '1', name: 'Litro', code: 'lt' } },
-      { label: 'Quilograma', value: { id: '2', name: 'Quilograma', code: 'kg' } },
-      { label: 'Unidade', value: { id: '3', name: 'Unidade', code: 'un' } }
-    ];
-
-    //validação do formulário
-    this.userform = this.fb.group({
-      'nomeItem': new FormControl('', Validators.compose(
-        [
-          Validators.required,
-          Validators.maxLength(50),
-          Validators.pattern('[a-zA-Z ]*'),
-        ])),
-      'unidadeMedida': new FormControl('', Validators.required),
-      'quantidade': new FormControl(''),
-      'preco': new FormControl('', Validators.required),
-      'perecivel': new FormControl(''),
-      'dataValidade': new FormControl('', Validators.required),
-      'dataFabricacao': new FormControl('', Validators.required)
-    });
-
     //navegação estrutural
     this.breadcrumbs = [
-      { label: 'New', icon: 'pi pi-fw pi-plus' },
-      { label: 'Open', icon: 'fa fa-list', url: 'https://en.wikipedia.org/wiki/Lionel_Messi' }
+      { label: 'Cadastro', icon: 'pi pi-fw pi-plus' },
+      { label: 'Listagem', icon: 'fa fa-list', url: 'https://en.wikipedia.org/wiki/Lionel_Messi' }
     ];
 
     //menu
@@ -96,38 +37,7 @@ export class AppComponent implements OnInit {
       { label: 'Cadastro', icon: 'pi pi-fw pi-plus' },
       { label: 'Listagem', icon: 'fa fa-list' }
     ];
-  }
-
-  //Trata formulário para antes de salvar no localstore
-  onSubmit(value: string) {
-    this.userform.value.quantidade = this.userform.value.quantidade.toString();
-    this.userform.value.preco = this.userform.value.preco.toString();
-    this.userform.value.perecivel = this.userform.value.perecivel.toString();
-    this.userform.value.dataFabricacao = this.userform.value.dataFabricacao.toLocaleDateString('pt-BR');
-    this.userform.value.dataValidade = this.userform.value.dataValidade.toLocaleDateString('pt-BR');
-    if (this.userform.value.perecivel === '')
-      this.userform.value.perecivel = 'false';
-
-    this.setLocalStore(JSON.stringify(this.userform.value))
-
-    this.submitted = true;
-    this.messageService.add({ severity: 'info', summary: 'Success', detail: 'Item incluido com sucesso' });
-    this.userform.reset();
-  }
-
-  // Salva dados na local store
-  setLocalStore(data: string) {
-    this.localStore.set(data);
-  }
-
-  // Busca dados na local store pela key
-  getLocalStore(key: string) {
-    return JSON.parse(this.localStore.get(key));
-  }
-
-  //Remove um item na local store
-  removeLocalStore(key: string) {
-    this.localStore.remove(key);
+    
   }
 
   getAllLocalStore() {
@@ -140,8 +50,7 @@ export class AppComponent implements OnInit {
       var item = JSON.parse(this.localStore.get(index.toString()))
       listItens.push(item)
     }
+    console.log(listItens)
     return listItens
   }
-
-  
 }
---
+++ src/app/item/form/form.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { FormComponent } from './form.component';
+
+@NgModule ({
+    declarations: [
+        FormComponent
+    ],
+    imports: [
+        
+    ],
+    exports: [
+        FormComponent
+    ]
+})
+export class FormModule {}
